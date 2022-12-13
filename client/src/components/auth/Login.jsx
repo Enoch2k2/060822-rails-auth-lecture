42a2695d1ac6e873a4a9c47a08f68421a93f55c6
Enoch2k2 <enoch2k2@hotmail.com>
2022-12-13 08:42:58 -0600

    added login / signup component

--- client/src/components/auth/Login.jsx
+++ client/src/components/auth/Login.jsx
@@ -1,8 +1,40 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const Login = () => {
+  const [username, setUsername] = useState("")
+  const [password, setPassword] = useState("")
+
+  const handleSubmit = e => {
+    e.preventDefault();
+  }
+
   return (
-    <div>Login</div>
+    <div>
+      <h2>Login</h2>
+      <form onSubmit={ handleSubmit }>
+        <div>
+          <label htmlFor="username">Username: </label>
+          <input 
+            type="text"
+            name="username"
+            id="username"
+            value={ username }
+            onChange={ e => setUsername(e.target.value) }
+            />
+        </div>
+        <div>
+          <label htmlFor="password">Password: </label>
+          <input 
+            type="password" 
+            name="password" 
+            id="password" 
+            value={ password }
+            onChange={ e => setPassword(e.target.value) }
+          />
+        </div>
+        <input type="submit" value="Login" />
+      </form>
+    </div>
   )
 }
 
--- client/src/components/auth/Signup.jsx
+++ client/src/components/auth/Signup.jsx
@@ -1,8 +1,40 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const Signup = () => {
+  const [username, setUsername] = useState("")
+  const [password, setPassword] = useState("")
+
+  const handleSubmit = e => {
+    e.preventDefault();
+  }
+
   return (
-    <div>Signup</div>
+    <div>
+      <h2>Create Account</h2>
+      <form onSubmit={ handleSubmit }>
+        <div>
+          <label htmlFor="username">Username: </label>
+          <input 
+            type="text"
+            name="username"
+            id="username"
+            value={ username }
+            onChange={ e => setUsername(e.target.value) }
+            />
+        </div>
+        <div>
+          <label htmlFor="password">Password: </label>
+          <input 
+            type="password" 
+            name="password" 
+            id="password" 
+            value={ password }
+            onChange={ e => setPassword(e.target.value) }
+          />
+        </div>
+        <input type="submit" value="Create Account" />
+      </form>
+    </div>
   )
 }
 
