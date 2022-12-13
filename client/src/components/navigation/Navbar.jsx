6506959f95883c734cd91fb0d91308882c425e25
Enoch2k2 <enoch2k2@hotmail.com>
2022-12-13 08:37:21 -0600

    added navigation

--- client/src/components/navigation/Navbar.jsx
+++ client/src/components/navigation/Navbar.jsx
@@ -1,8 +1,13 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 const Navbar = () => {
   return (
-    <div>Navbar</div>
+    <ul>
+      <li><Link to="/">Home</Link></li>
+      <li><Link to="/signup">Signup</Link></li>
+      <li><Link to="/login">Login</Link></li>
+    </ul>
   )
 }
 
